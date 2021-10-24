6c7e28b2675781901f1407dc8fde8173db6a4af1
walter <tjcxs136720@163.com>
2021-10-24 14:11:20 +0800

    Complete lab2-ex1

--- src/java/simpledb/JoinPredicate.java
+++ src/java/simpledb/JoinPredicate.java
@@ -10,6 +10,7 @@ public class JoinPredicate implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+
     /**
      * Constructor -- create a new predicate over two fields of two tuples.
      * 
@@ -24,8 +25,16 @@ public class JoinPredicate implements Serializable {
      *            Predicate.Op.LESS_THAN_OR_EQ
      * @see Predicate
      */
+
+    private final int field1;
+    private final Predicate.Op op;
+    private final int field2;
+
     public JoinPredicate(int field1, Predicate.Op op, int field2) {
         // some code goes here
+        this.field1 = field1;
+        this.op = op;
+        this.field2 = field2;
     }
 
     /**
@@ -36,24 +45,24 @@ public class JoinPredicate implements Serializable {
      */
     public boolean filter(Tuple t1, Tuple t2) {
         // some code goes here
-        return false;
+        return t1.getField(field1).compare(op, t2.getField(field2));
     }
     
     public int getField1()
     {
         // some code goes here
-        return -1;
+        return field1;
     }
     
     public int getField2()
     {
         // some code goes here
-        return -1;
+        return field2;
     }
     
     public Predicate.Op getOperator()
     {
         // some code goes here
-        return null;
+        return op;
     }
 }
